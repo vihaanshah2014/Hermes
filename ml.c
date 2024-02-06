acaad2605355243dd9dd03708f85afd1037d623c
vihaanshah2014 <58313595+vihaanshah2014@users.noreply.github.com>
2024-02-05 20:26:27 -0500

    method 1

--- ml.c
+++ ml.c
@@ -3,3 +3,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+double** allocateMatrix(int rows, int cols){
+    double **matrix = malloc(rows * sizeof(double*));
+
+    for(int i = 0; i < rows; i++){
+        matrix[i] = malloc(cols * sizeof(double));
+    }
+
+    return matrix;
+}
