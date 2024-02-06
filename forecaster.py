e07235f71cb3d88c806299e2ad8706438767c8d1
vihaanshah2014 <58313595+vihaanshah2014@users.noreply.github.com>
2024-02-06 18:05:50 -0500

    prob 2

--- forecaster.py
+++ forecaster.py
@@ -1,12 +1,10 @@
+python
 # Define the actual sales and forecast lists
 actual_sales = [4035, 4335, 4410, 4170, 4020, 4035, 3900, 4143, 6456, 6942, 7056, 6672]
-forecast = [3825, 3990, 4650, 4260, 3750, 4020, 4215, 4065, 6300, 6405, 7272, 6816]
+forecast = [4320, 4305, 4080, 4470, 4410, 4050, 3585, 4131, 6732, 6888, 6708, 7137]
 
 # Calculate the absolute differences
-absolute_differences = []
-for actual, forecast in zip(actual_sales, forecast):
-    absolute_difference = abs(actual - forecast)
-    absolute_differences.append(absolute_difference)
+absolute_differences = [abs(actual - forecast) for actual, forecast in zip(actual_sales, forecast)]
 
 # Show the work for absolute differences
 print("Absolute Differences for each month:")
