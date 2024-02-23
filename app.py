22b47a2c6f0d96e87975c50e420c65432a46963a
vihaanshah2014 <58313595+vihaanshah2014@users.noreply.github.com>
2024-02-23 16:10:46 -0500

    testing

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def predict():
     # Example hardcoded values for demonstration
     symbol = 'SPY'
     start_date = '2022-01-01'
-    end_date = '2024-02-22'
+    end_date = '2024-02-23'
     prediction_days = 60
 
     # Load and prepare data
